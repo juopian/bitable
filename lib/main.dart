277920cc4877e7a6a1bd6df32daf2dd55fe1b724
juopian <juopian@gmail.com>
2020-12-19 12:23:10 +0800

    multi level header

--- lib/main.dart
+++ lib/main.dart
@@ -53,11 +53,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 const int rows = 30;
-const int cols = 15;
+const int cols = 10;
 const double headerHeight = 40;
 const double contentHeight = 40;
-const double contentWidth = 60;
-const double sideWidth = 100;
+const double contentWidth = 80;
+const double sideWidth = 80;
 
 class _MyHomePageState extends State<MyHomePage> {
   ScrollController _scrollController1;
@@ -66,7 +66,367 @@ class _MyHomePageState extends State<MyHomePage> {
   ScrollController _scrollController4;
 
   int initScroller;
+  int headerLevel = 3;
+
+  /*
+  header data struct
+   */
+  Map<String, String> headerData = {
+    "act1": '区域 月度',
+    "act2": '区域 季度',
+    "col1": '总计',
+    "col2": '全省 深圳 罗湖',
+    "col3": '全省 深圳 宝安',
+    "col4": '全省 广州 天河',
+    "col5": '全省 广州 海珠',
+    "col6": '全省 广州 番禺',
+    "col7": '广州 荔湾',
+    "col8": '天河 营服1',
+    "col9": '天河 营服2',
+    "col10": '全省 广州 萝岗',
+  };
+
+  List<Map<String, dynamic>> mainData = [
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+    {
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },{
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },{
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },{
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },{
+      "act1": '100',
+      "act2": '200',
+      "col1": '1',
+      "col2": '2',
+      "col3": '3',
+      "col4": '4',
+      "col5": '5',
+      "col6": '6',
+      "col7": '7',
+      "col8": '8',
+      "col9": '9',
+      "col10": '10',
+    },
+  ];
+
+  Widget _slot(List<Map<String, String>> d) {
+    /*
+     d like
+     [{col2: 全省 深圳 罗湖},
+      {col3: 全省 深圳 宝安},
+      {col4: 全省 广州 天河},
+      {col5: 全省 广州 海珠},
+      {col6: 全省 广州 番禺}]
+     */
+    List<Widget> children = [];
+    int level = 0;
+    int maxLevel = d.first.values.first.split(' ').length;
+    String lastValue;
+    while (level < maxLevel) {
+      List<Widget> levelRow = [];
+      List<String> inner = [];
+      lastValue = null;
+      d.forEach((Map<String, String> element) {
+        String loopTitle = element.values.first.split(' ')[level];
+        if (lastValue != loopTitle) {
+          if (inner.length > 0) {
+            levelRow.add(Item(
+              color: Colors.blue,
+              height: headerHeight *
+                  (level == 0 ? (headerLevel - maxLevel + 1) : 1),
+              width: contentWidth * inner.length,
+              child: Center(child: Text(lastValue)),
+            ));
+            inner = [loopTitle];
+          } else {
+            inner.add(loopTitle);
+          }
+          lastValue = loopTitle;
+        } else {
+          inner.add(loopTitle);
+        }
+      });
+
+      if (inner.length > 0) {
+        levelRow.add(Item(
+          color: Colors.blue,
+          height:
+              headerHeight * (level == 0 ? (headerLevel - maxLevel + 1) : 1),
+          width: contentWidth * inner.length,
+          child: Center(child: Text(lastValue)),
+        ));
+      }
+
+      children.add(Row(
+        children: levelRow,
+      ));
+
+      level++;
+    }
+
+    return Column(
+      children: children,
+    );
+  }
+
   Widget _header() {
+    List<String> sideKeys = headerData.keys.toList();
+
+    List<List<Map<String, String>>> leftSide = [];
+    List<List<Map<String, String>>> rightSide = [];
+    String lastTopLevelTitle;
+    headerData.forEach((key, value) {
+      String t = value.split(' ').first;
+      RegExp r = new RegExp(r'^(act|dim)');
+      if (lastTopLevelTitle != t) {
+        if (key.startsWith(r)) {
+          leftSide.add([
+            {key: value}
+          ]);
+        } else {
+          rightSide.add([
+            {key: value}
+          ]);
+        }
+        lastTopLevelTitle = t;
+      } else {
+        if (key.startsWith(r)) {
+          leftSide.last.add({key: value});
+        } else {
+          rightSide.last.add({key: value});
+        }
+      }
+    });
+    print('left is $leftSide, right is $rightSide');
+    _slot(rightSide[4]);
     return NotificationListener<ScrollEndNotification>(
       onNotification: (notification) {
         initScroller = null;
@@ -74,12 +434,15 @@ class _MyHomePageState extends State<MyHomePage> {
       },
       child: Row(
         children: [
-          Item(
-            color: Colors.blue,
-            width: sideWidth,
-            height: headerHeight,
-            child: Center(child: Text('区域')),
+          Row(
+            children: leftSide.map((e) => _slot(e)).toList(),
           ),
+//          Item(
+//            color: Colors.blue,
+//            width: sideWidth,
+//            height: headerHeight * maxLevel,
+//            child: Center(child: Text('区域')),
+//          ),
           Expanded(child: LayoutBuilder(builder: (context, constraints) {
             if (constraints.maxWidth > contentWidth * cols)
               return Row(
@@ -93,20 +456,23 @@ class _MyHomePageState extends State<MyHomePage> {
                     .toList(),
               );
             return SingleChildScrollView(
-              physics: ClampingScrollPhysics(),
-              controller: _scrollController1,
-              scrollDirection: Axis.horizontal,
-              child: Row(
-                children: List<int>.generate(cols, (int index) => index)
-                    .map((e) => Item(
-                          color: Colors.blue,
-                          height: headerHeight,
-                          width: contentWidth,
-                          child: Center(child: Text('col-$e')),
-                        ))
-                    .toList(),
-              ),
-            );
+                physics: ClampingScrollPhysics(),
+                controller: _scrollController1,
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: rightSide.map((e) => _slot(e)).toList(),
+                )
+//              Row(
+//                children: List<int>.generate(cols, (int index) => index)
+//                    .map((e) => Item(
+//                          color: Colors.blue,
+//                          height: headerHeight,
+//                          width: contentWidth,
+//                          child: Center(child: Text('col-$e')),
+//                        ))
+//                    .toList(),
+//              ),
+                );
           }))
         ],
       ),
@@ -130,15 +496,27 @@ class _MyHomePageState extends State<MyHomePage> {
                 physics: ClampingScrollPhysics(),
                 scrollDirection: Axis.vertical,
                 child: Column(
-                  children: List<int>.generate(rows, (int index) => index)
-                      .map((e) => Item(
-                            color: Colors.orange,
-                            width: sideWidth,
-                            height: contentHeight,
-                            child: Center(child: Text('row-$e')),
-                          ))
-                      .toList(),
-                ),
+                    children: mainData.map((e) {
+                  return Row(
+                    children: ['act1', 'act2']
+                        .map((col) => Item(
+                              color: Colors.orange,
+                              width: sideWidth,
+                              height: contentHeight,
+                              child: Center(child: Text(e[col])),
+                            ))
+                        .toList(),
+                  );
+                }).toList()
+//                    List<int>.generate(rows, (int index) => index)
+//                      .map((e) => Item(
+//                            color: Colors.orange,
+//                            width: sideWidth,
+//                            height: contentHeight,
+//                            child: Center(child: Text('row-$e')),
+//                          ))
+//                      .toList(),
+                    ),
               );
             },
           ),
@@ -175,21 +553,44 @@ class _MyHomePageState extends State<MyHomePage> {
                       physics: ClampingScrollPhysics(),
                       scrollDirection: Axis.vertical,
                       child: Column(
-                        children: List<int>.generate(rows, (int index) => index)
-                            .map((e) => Row(
-                                  children: List<int>.generate(
-                                          cols, (int index) => index)
-                                      .map((e1) => Item(
-                                            color: Colors.orange,
-                                            height: contentHeight,
-                                            width: contentWidth,
-                                            child:
-                                                Center(child: Text('$e - $e1')),
-                                          ))
-                                      .toList(),
-                                ))
-                            .toList(),
-                      )),
+                          children: mainData.map((e) {
+                        return Row(
+                          children: [
+                            'col1',
+                            'col2',
+                            'col3',
+                            'col4',
+                            'col5',
+                            'col6',
+                            'col7',
+                            'col8',
+                            'col9',
+                            'col10',
+                          ]
+                              .map((col) => Item(
+                                    color: Colors.orange,
+                                    width: sideWidth,
+                                    height: contentHeight,
+                                    child: Center(child: Text(e[col])),
+                                  ))
+                              .toList(),
+                        );
+                      }).toList()
+//                        List<int>.generate(rows, (int index) => index)
+//                            .map((e) => Row(
+//                                  children: List<int>.generate(
+//                                          cols, (int index) => index)
+//                                      .map((e1) => Item(
+//                                            color: Colors.orange,
+//                                            height: contentHeight,
+//                                            width: contentWidth,
+//                                            child:
+//                                                Center(child: Text('$e - $e1')),
+//                                          ))
+//                                      .toList(),
+//                                ))
+//                            .toList(),
+                          )),
                 );
               },
             ),
@@ -238,7 +639,7 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Column(
         children: [
-          SizedBox(height: 400, child: SimpleLineChart.withSampleData()),
+          SizedBox(height: 300, child: SimpleLineChart.withSampleData()),
           _header(),
           Expanded(child: _body())
         ],
